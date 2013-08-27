84333a5184f8fb83cf94c961878e870794c35183
Silviu Durduc <iamsilviu@gmail.com>
2013-08-27 11:33:38 +0300

    Create UtilityBelt-100.php

---
+++ UB/PHP/UtilityBelt-100.php
@@ -0,0 +1,17 @@
+<?php
+  /**
+   * UtilityBelt Class
+   *
+   * Common helpers
+   *
+   * @version 1.0.0
+   * @package	UB
+   * @category	Utility
+   * @author	Durduc Silviu iamsilviu@gmail.com
+   */
+   
+   class UtilityBelt {
+   
+   }
+?>
+/* End of file UtilityBelt.php */
