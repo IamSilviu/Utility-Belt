1eb67db2ca0e0063a2c951d883f2d2be98d76bb0
Silviu Durduc <iamsilviu@gmail.com>
2013-12-16 14:45:55 +0200

    Update UtilityBelt-1.0.0.js

--- UB/JS/UtilityBelt-1.0.0.js
+++ UB/JS/UtilityBelt-1.0.0.js
@@ -1,44 +1,50 @@
-/*
+/**
+ *
+ * Utility Belt
+ *
+ */
+if (typeof UtilityBelt === 'undefined') UtilityBelt = UB = {};
 
-Utility Belt
+UtilityBelt = UB = (function () {
 
-*/
+    /**
+     * Private
+     */
 
-if(typeof UtilityBelt === 'undefined')  UtilityBelt = UB = {};
+    return {
+        /**
+         * @cfg regExp
+         * Regular expression library
+         */
+        regExp: {
+            email : /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        },
 
-UtilityBelt = UB = (function(){
-  
-  /**
-   * @cfg regExp
-   * Regular expression library
-   */
-   regExp : {
-     email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-   },
-   
-   /**
-   * @Method getRegExp
-   * Getter for Regular Expression library
-   * @param email {string}
-   * @return boolean
-   */  
-   getRegExp: function(exp){
-     
-     if(!this.regExp[exp]){
-       consol.warn('The regular expression',exp,'was not found');
-       return null;
-     } 
-   
-     return this.regExp[exp] ? this.regExp[exp] : null;
-   },
-   
-  /**
-   * @Method isEmail
-   * Validate string as email pattern
-   * @param email {string}
-   * @return boolean
-   */
-  isEmail : function(email){
-  	return this.regEx.email.test(email);
-  }
-});
+        /**
+         * @Method getRegExp
+         * Getter for Regular Expression library
+         * @param email {string}
+         * @return boolean
+         */
+        getRegExp: function (exp) {
+
+            if (!this.regExp[exp]) {
+                consol.warn('The regular expression', exp, 'was not found');
+                return null;
+            }
+
+            return this.regExp[exp] ? this.regExp[exp] : null;
+        },
+
+        /**
+         * @Method isEmail
+         * Validate string as email pattern
+         * @param email {string}
+         * @return boolean
+         */
+        isEmail: function (email) {
+            return this.getRegExp('email').test(email);
+        }
+
+    }
+})();
