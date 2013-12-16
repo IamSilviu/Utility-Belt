764fc0cd16c4a624cffea88646e7519c8074bdcc
Silviu Durduc <iamsilviu@gmail.com>
2013-12-16 14:41:06 +0200

    Update UtilityBelt-1.0.0.js

--- UB/JS/UtilityBelt-1.0.0.js
+++ UB/JS/UtilityBelt-1.0.0.js
@@ -1,8 +1,44 @@
+/*
+
+Utility Belt
+
+*/
+
 if(typeof UtilityBelt === 'undefined')  UtilityBelt = UB = {};
 
-UtilityBelt = UB = {
+UtilityBelt = UB = (function(){
+  
+  /**
+   * @cfg regExp
+   * Regular expression library
+   */
+   regExp : {
+     email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+   },
+   
+   /**
+   * @Method getRegExp
+   * Getter for Regular Expression library
+   * @param email {string}
+   * @return boolean
+   */  
+   getRegExp: function(exp){
+     
+     if(!this.regExp[exp]){
+       consol.warn('The regular expression',exp,'was not found');
+       return null;
+     } 
+   
+     return this.regExp[exp] ? this.regExp[exp] : null;
+   },
+   
+  /**
+   * @Method isEmail
+   * Validate string as email pattern
+   * @param email {string}
+   * @return boolean
+   */
   isEmail : function(email){
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  	return re.test(email);
+  	return this.regEx.email.test(email);
   }
-}
+});
